53dee3b6b80c20075cf7d0451266c30850924038
ANKIT99863 <ankitjaiswal.5074@gmail.com>
2023-11-26 13:25:11 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import ajax from './ajax'
+const BASE = 'http://localhost:3000'
+//login
+
+export const reqLogin = (username,password) => ajax(BASE+'/Login',{username,password},'POST')
+
+//Registered users
+export const reqRegister = (username,password,email) => ajax(BASE+'/Login',{username,password,email},'POST')
---
+++ src/utils/memoryUtils.js
@@ -0,0 +1,4 @@
+export default {
+    user:{},//Save the username who is currently logged in
+    imgUrl:"",
+}
